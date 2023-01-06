602e2e587e2966a43440d94a306f61a42abded16
glenchiridza <glenchiridz@gmail.com>
2023-01-07 00:41:20 +0200

    add gitignore

--- django_enterprise_dev/settings.py
+++ django_enterprise_dev/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import posixpath
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -112,5 +114,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = posixpath.join(
+    *(BASE_DIR.split(os.path.sep) + ['static'])
+)
+
+MEDIA_URL = '/=media/'
+MEDIA_ROOT = posixpath.join(
+    *(BASE_DIR.split(os.path.sep)+['media'])
+)
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
