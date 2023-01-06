1079e26b8603c870c190bccf570eb72c150c7713
glenchiridza <glenchiridz@gmail.com>
2023-01-07 01:05:36 +0200

    Add chapter3 app

--- django_enterprise_dev/settings.py
+++ django_enterprise_dev/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'chapter_2.apps.Chapter2Config',
+    'chapter_3.apps.Chapter3Config',
 ]
 
 MIDDLEWARE = [
