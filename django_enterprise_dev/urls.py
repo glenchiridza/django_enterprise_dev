473c3e6e3b5c6f935455a75d2a4697286ea324b4
glenchiridza <glenchiridz@gmail.com>
2023-01-16 11:15:32 +0200

    template views

--- django_enterprise_dev/urls.py
+++ django_enterprise_dev/urls.py
@@ -18,8 +18,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='chapter_4/index.html')
+         ,kwargs ={'sub_title':'This is a sub title passed as kwargs'}),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # uncomment when debug == false
